f778e7d09ca3fa5e939a12b5c098b72bedba74c5
Lisa Miller <ljmiller@hawaii.edu>
2024-01-28 15:47:58 -1000

    Update and cleanup

--- FractionInterface.java
+++ FractionInterface.java
@@ -1,73 +1,73 @@
 /**
- * Interface is a list of method prototypes that other classes must implement (like a contract)
- * 
+ * A list of method prototypes that other classes must implement.
+ * All methods are by default public, no need to list
  * @author William McDaniel Albritton
  */
 public interface FractionInterface {
 
-  /**
-   * This Adds Two Fractions Together.
-   * 
-   * @param fraction2 Is The 2nd Fraction.
-   * @return a 3rd Fraction The Is The Result
-   */
-  public FractionInterface add(FractionInterface fraction2);
+   /**
+    * This Adds Two Fractions Together.
+    * 
+    * @param fraction2 Is The 2nd Fraction.
+    * @return a 3rd Fraction The Is The Result
+    */
+   FractionInterface add(FractionInterface fraction2);
 
-  /**
-   * This Subtracts Two Fractions Together.
-   * 
-   * @param fraction2 Is The 2nd Fraction.
-   * @return a 3rd Fraction The Is The Result
-   */
-  public FractionInterface subtract(FractionInterface fraction2);
+   /**
+    * This Subtracts Two Fractions Together.
+    * 
+    * @param fraction2 Is The 2nd Fraction.
+    * @return a 3rd Fraction The Is The Result
+    */
+   FractionInterface subtract(FractionInterface fraction2);
 
-  /**
-   * This Multiplies Two Fractions .
-   * 
-   * @param fraction2 Is The 2nd Fraction.
-   * @return a 3rd Fraction The Is The Product
-   */
-  public FractionInterface multiply(FractionInterface fraction2);
+   /**
+    * This Multiplies Two Fractions .
+    * 
+    * @param fraction2 Is The 2nd Fraction.
+    * @return a 3rd Fraction The Is The Product
+    */
+   FractionInterface multiply(FractionInterface fraction2);
 
-  /**
-   * This Divides Two Fractions .
-   * 
-   * @param fraction2 Is The 2nd Fraction.
-   * @return a 3rd Fraction The Is The Quotient
-   */
-  public FractionInterface divide(FractionInterface fraction2);
+   /**
+    * This Divides Two Fractions .
+    * 
+    * @param fraction2 Is The 2nd Fraction.
+    * @return a 3rd Fraction The Is The Quotient
+    */
+   FractionInterface divide(FractionInterface fraction2);
 
-  /** This method reduces a fraction to its lowest terms. */
-  public void reduceToLowestTerms();
+   /** This method reduces a fraction to its lowest terms. */
 
-  /**
-   * This Is An "Accessor" Method - Used To Get A Data Field.
-   * 
-   * @return the numerator
-   */
-  public Integer getNumerator();
 
-  /**
-   * This Is An "Accessor" Method - Used To Get A Data Field.
-   * 
-   * @return the denominator
-   */
-  public Integer getDenominator();
+   /**
+    * This Is An "Accessor" Method - Used To Get A Data Field.
+    * 
+    * @return the numerator
+    */
+   Integer getNumerator();
 
-  /**
-   * This Is A "Mutator" Method - Used To Set A Data Field.
-   * 
-   * @param numeratorParameter is the first name.
-   */
-  public void setNumerator(Integer numeratorParameter);
+   /**
+    * This Is An "Accessor" Method - Used To Get A Data Field.
+    * 
+    * @return the denominator
+    */
+   Integer getDenominator();
 
-  /**
-   * This Is A "Mutator" Method - Used To Set A Data Field.
-   * 
-   * @param lastNameParameter is the last name.
-   * @exception IllegalFractionException if the denominator is zero
-   */
-  public void setDenominator(Integer denominatorParameter) throws IllegalFractionException;
+   /**
+    * This Is A "Mutator" Method - Used To Set A Data Field.
+    * 
+    * @param newNumerator is the first name.
+    */
+   void setNumerator(Integer newNumerator);
 
-}// End of interface.
+   /**
+    * This Is A "Mutator" Method - Used To Set A Data Field.
+    * 
+    * @param newDenominator is the denominator.
+    * @exception IllegalFractionException if the denominator is zero
+    */
+   void setDenominator(Integer newDenominator) throws IllegalFractionException;
+
+} // End of interface.
 
--- IllegalFractionException.java
+++ IllegalFractionException.java
@@ -1,16 +1,16 @@
 /**
- * An Exception for use with the Fraction class
+ * An Exception for use with the Fraction class.
  * 
  * @author William McDaniel Albritton
  */
 public class IllegalFractionException extends RuntimeException {
-  /**
-   * Constructor
-   * 
-   * @param message Describes the cause of the error
-   */
-  public IllegalFractionException(String message) {
-    super(message);
-  }
-}// end of class
+   /**
+    * Constructor.
+    * 
+    * @param message Describes the cause of the error
+    */
+   public IllegalFractionException(String message) {
+      super(message);
+   }
+} // end of class
 
